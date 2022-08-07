b34f4bfca3adff1962f559d18ac2a52216fce6fa
Blasco <blascokoa1@gmail.com>
2022-08-07 10:14:54 +0200

    clean code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,18 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SharedModule } from './shared/shared.module';
-import { WalletModule } from './wallet/wallet.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { NftModule } from './nft/nft.module';
 import process from 'process';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // SharedModule,
-    // WalletModule,
-    // NftModule,
     // MongooseModule.forRootAsync({
     //   useFactory: () => ({ uri: process.env.MONGO_URI }),
     // }),
--- backend/src/wallet/wallet.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WalletResourceService } from './wallet.service';
-import { WalletController } from './wallet.controller';
-
-@Module({
-  controllers: [WalletController],
-  providers: [WalletResourceService],
-})
-export class WalletModule {}
