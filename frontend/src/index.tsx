880c049b5b698a9aa5c9b4d99b3e7e7ca284f956
Sinan Gultekin <sg.sinangultekin@gmail.com>
2022-08-07 16:25:24 +0300

    Connect wallet part completed.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { DAppProvider } from '@usedapp/core';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <DAppProvider config={{}}>
+      <App />
+    </DAppProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
