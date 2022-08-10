5236af9fa76259874fafee6cb4d1e0fe6f8883cc
Blasco <blascokoa1@gmail.com>
2022-08-11 01:57:32 +0200

    fix: show tx id

--- frontend/src/components/mint-form/Form.tsx
+++ frontend/src/components/mint-form/Form.tsx
@@ -1,16 +1,22 @@
-import { ethers } from 'ethers';
-import { useContext, useState } from 'react';
-import { FileUploader } from 'react-drag-drop-files';
-import BlockchainService from '../../services/Blockchain.service';
-import EndpointService from '../../services/Endpoint.service';
-import { AccountContext } from '../wrappers/IdentityWrapper';
-import { DescriptionInput } from './DescriptionInput';
-import { BAD_ADDRESS, getDefaultKudoWithWalletAddress, fileTypes } from './formConstants';
-import { InputField } from './InputField';
-import { SubmitButton } from './SubmitButton';
+import { ethers } from "ethers";
+import { useContext, useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import BlockchainService from "../../services/Blockchain.service";
+import EndpointService from "../../services/Endpoint.service";
+import { AccountContext } from "../wrappers/IdentityWrapper";
+import { DescriptionInput } from "./DescriptionInput";
+import {
+  BAD_ADDRESS,
+  getDefaultKudoWithWalletAddress,
+  fileTypes,
+} from "./formConstants";
+import { InputField } from "./InputField";
+import { SubmitButton } from "./SubmitButton";
 
 export function Form(props: { title: string; walletAdd: string }): JSX.Element {
-  const [dto, setDto] = useState(getDefaultKudoWithWalletAddress(props.walletAdd));
+  const [dto, setDto] = useState(
+    getDefaultKudoWithWalletAddress(props.walletAdd)
+  );
   const [ownAddress, setOwnAddress] = useState(BAD_ADDRESS);
   const [file, setFile] = useState(null as unknown as Blob);
 
@@ -22,7 +28,7 @@ export function Form(props: { title: string; walletAdd: string }): JSX.Element {
   try {
     context.getAddress().then((v) => setOwnAddress(v));
   } catch (error) {
-    console.log('not able to retrieve address yet');
+    console.log("not able to retrieve address yet");
   }
 
   const handleChange = (e: any) => {
@@ -37,12 +43,13 @@ export function Form(props: { title: string; walletAdd: string }): JSX.Element {
 
   const submitCallback = () => {
     EndpointService.postImage(file, ownAddress).then((url: string) => {
-      console.log('successful file upload: ', url);
+      console.log("successful file upload: ", url);
       dto.imageUrl = url;
       BlockchainService.deployNft(dto, signer).then((response: any) => {
-        console.log('successful deployment: ', response);
+        console.log("successful deployment: ", response);
+        dto.tokenId = response.hash;
         EndpointService.saveKudo(dto).then((response: string) => {
-          console.log('successful saving of the kudo as an NFT,', response);
+          console.log("successful saving of the kudo as an NFT,", response);
         });
       });
     });
@@ -51,7 +58,9 @@ export function Form(props: { title: string; walletAdd: string }): JSX.Element {
   return (
     <>
       <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-        <h6 className="font-medium leading-tight text-base mt-0 mb-2 text-blue-600">{props.title}</h6>
+        <h6 className="font-medium leading-tight text-base mt-0 mb-2 text-blue-600">
+          {props.title}
+        </h6>
         <form>
           <InputField
             placeholder="from"
@@ -61,7 +70,11 @@ export function Form(props: { title: string; walletAdd: string }): JSX.Element {
             disabled
           />
           <InputField placeholder="to" value={dto.to} onChange={handleChange} />
-          <InputField placeholder="name" value={dto.name} onChange={handleChange} />
+          <InputField
+            placeholder="name"
+            value={dto.name}
+            onChange={handleChange}
+          />
           <DescriptionInput callback={handleChange} />
           <FileUploader
             className="
@@ -89,7 +102,10 @@ export function Form(props: { title: string; walletAdd: string }): JSX.Element {
             for now it'll live on the Ropsten network
           </small>
           {ownAddress === BAD_ADDRESS && <p>First connect with your wallet!</p>}
-          <SubmitButton disabled={ownAddress === BAD_ADDRESS || !file} callback={submitCallback} />
+          <SubmitButton
+            disabled={ownAddress === BAD_ADDRESS || !file}
+            callback={submitCallback}
+          />
         </form>
       </div>
     </>
