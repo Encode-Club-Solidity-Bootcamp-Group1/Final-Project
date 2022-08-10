2fef22e832f1e636d4d422c3609598c687df2f73
Blasco <blascokoa1@gmail.com>
2022-08-10 12:49:48 +0200

    style: fix styles

--- frontend/src/pages/home-page/home-page.tsx
+++ frontend/src/pages/home-page/home-page.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box } from '@mui/system';
+import { Box } from "@mui/system";
 import ConnectWallet from "../../components/connect-wallet";
 import { Form } from "../../components/mint-form/Form";
 import MainPage from "../main-page";
@@ -12,15 +12,24 @@ export default function HomePage() {
   return (
     <MainPage>
       <div className="grid place-items-center h-screen relative">
-        <nav >
-          <ConnectWallet setWalletAdd={setWalletAdd}/>
-        </nav >
-        <Box sx={{ flexDirection: 'row' }}>
-          <Form title='Mint NFT' walletAdd={walletAdd}/>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row-reverse",
+            mt: 2,
+            mr: 4,
+            width: "100%",
+          }}
+        >
+          <ConnectWallet setWalletAdd={setWalletAdd} />
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: "row" }}>
+          <Box sx={{ mx: 2 }}>
+            <Form title="Mint NFT" walletAdd={walletAdd} />
+          </Box>
           <ListsPanel />
         </Box>
       </div>
     </MainPage>
   );
 }
-
